92b6f616f41dd684df03e1ea31e3017d8aaddd1e
anthoasho <anthoasho@gmail.com>
2019-07-25 21:01:39 +0900

    incorporate backend

---
+++ morikin/precache-manifest.6c3071d0befef34d37f25ae0ce46d21c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354c6eab4852d800d0e1cfbf4f91b74e",
+    "url": "https://anthonyashurst.com/morikin/index.html"
+  },
+  {
+    "revision": "69c005eb10e1321be4f4",
+    "url": "https://anthonyashurst.com/morikin/static/css/main.503c4789.chunk.css"
+  },
+  {
+    "revision": "fd8f6d53c6b68f343494",
+    "url": "https://anthonyashurst.com/morikin/static/js/2.b731feb9.chunk.js"
+  },
+  {
+    "revision": "69c005eb10e1321be4f4",
+    "url": "https://anthonyashurst.com/morikin/static/js/main.f77896b8.chunk.js"
+  },
+  {
+    "revision": "2b4b1b6128924df9a290",
+    "url": "https://anthonyashurst.com/morikin/static/js/runtime~main.f863216b.js"
+  },
+  {
+    "revision": "b6931a333011a63d6097e4768699f4cf",
+    "url": "https://anthonyashurst.com/morikin/static/media/logo.b6931a33.svg"
+  },
+  {
+    "revision": "e28ca059e379cec77cf9ab758f96b86d",
+    "url": "https://anthonyashurst.com/morikin/static/media/morikin-sh.e28ca059.jpg"
+  }
+]);
