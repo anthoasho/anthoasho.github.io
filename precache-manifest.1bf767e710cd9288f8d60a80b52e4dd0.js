83e8aff480c5975544e8f876de68bd2d0fad29b4
anthoasho <anthoasho@gmail.com>
2019-08-15 19:28:17 +0900

    new portfolio desgin

---
+++ precache-manifest.1bf767e710cd9288f8d60a80b52e4dd0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79117493cd088563ecae104fa76dd0ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8dcc89c33dd45946ce8",
+    "url": "/static/css/main.a8aadf48.chunk.css"
+  },
+  {
+    "revision": "c2785ec678e3dc7bbbde",
+    "url": "/static/js/2.5078075b.chunk.js"
+  },
+  {
+    "revision": "b8dcc89c33dd45946ce8",
+    "url": "/static/js/main.9eaaeb8c.chunk.js"
+  },
+  {
+    "revision": "c21874acebccd0a250a0",
+    "url": "/static/js/runtime~main.a2086642.js"
+  },
+  {
+    "revision": "af67f564ab1033bc17fc0603a7ec594b",
+    "url": "/static/media/clock.af67f564.png"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "b0727a5a48177510e69e0d89d20829e8",
+    "url": "/static/media/durham.b0727a5a.svg"
+  },
+  {
+    "revision": "926fc517501a267d33186e6d2ff2a26a",
+    "url": "/static/media/graduate.926fc517.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/static/media/html5.676e8188.svg"
+  },
+  {
+    "revision": "4e51436d7f746d9f42ac11c594b50ab6",
+    "url": "/static/media/jcamp.4e51436d.png"
+  },
+  {
+    "revision": "80639bbf1a6828056bee3603d1eea563",
+    "url": "/static/media/jquery.80639bbf.svg"
+  },
+  {
+    "revision": "65d3b011daeb7e1cabfdf87eca2d0c2b",
+    "url": "/static/media/lightsout.65d3b011.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "2369213f1be069f87db04f23efc81d74",
+    "url": "/static/media/morikin.2369213f.png"
+  },
+  {
+    "revision": "cc2b3aafefa9bd0890b1fcc6b9ca835f",
+    "url": "/static/media/nodejs.cc2b3aaf.svg"
+  },
+  {
+    "revision": "9deae8fba1605e1f276a60c3dc2b2583",
+    "url": "/static/media/profile.9deae8fb.png"
+  },
+  {
+    "revision": "2a3140919b230010de35c8ebfd13bfb1",
+    "url": "/static/media/rgb-game.2a314091.png"
+  },
+  {
+    "revision": "93c11b6454097d873c2dc46b0d515fbe",
+    "url": "/static/media/socialme.93c11b64.jpg"
+  }
+]);
