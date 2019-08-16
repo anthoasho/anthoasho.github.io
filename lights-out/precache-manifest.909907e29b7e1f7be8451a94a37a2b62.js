0c08f89a6080ba326db0d08754096653d4f523ab
anthoasho <anthoasho@gmail.com>
2019-08-16 14:31:43 +0900

    fixed url issue lights out

---
+++ lights-out/precache-manifest.909907e29b7e1f7be8451a94a37a2b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6b4aa3c0534f92074b7a0ece0cab2b",
+    "url": "/rgb-game/index.html"
+  },
+  {
+    "revision": "49d767686073f9c0ec13",
+    "url": "/rgb-game/static/css/main.37b63f46.chunk.css"
+  },
+  {
+    "revision": "4781d78766856202e746",
+    "url": "/rgb-game/static/js/2.75832e6e.chunk.js"
+  },
+  {
+    "revision": "49d767686073f9c0ec13",
+    "url": "/rgb-game/static/js/main.52ff5947.chunk.js"
+  },
+  {
+    "revision": "d5ff252993884186ae4b",
+    "url": "/rgb-game/static/js/runtime~main.a2518df0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rgb-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- lights-out/service-worker.js
+++ lights-out/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.76425a89a7254bffa9f44a71dbb93336.js"
+  "/rgb-game/precache-manifest.909907e29b7e1f7be8451a94a37a2b62.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rgb-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
